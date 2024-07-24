f8e02be54a829a58243877a67934a50046cb0364
StefaniaBuri <60428937+StefaniaBuri@users.noreply.github.com>
2024-07-24 20:39:03 +0200

    Creando Agenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from "./components/cabecera/cabecera.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { PieComponent } from "./components/pie/pie.component";
+import { ContenedorComponent } from "./components/contenedor/contenedor.component";
+import { TareaComponent } from "./components/tarea/tarea.component";
 
-@Component({
+
+@Component({ // decorador de la clase Component, lo que esta dentro de este codigo es un objeto
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceraComponent, FormularioComponent, PieComponent, ContenedorComponent, TareaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
